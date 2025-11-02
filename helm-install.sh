42f729c521a1dc012943906a6c1977f2db5e62e3
spenceraf19 <satlantic@damienamir.localdomain>
2025-11-02 06:50:46 -0800

    All pods running

--- helm-install.sh
+++ helm-install.sh
@@ -2,5 +2,7 @@
 kubectl apply -f flyte-namespace.yaml
 helm repo add flyteorg https://flyteorg.github.io/flyte
 helm repo update
+kubectl apply -f storage-minio.yaml
+kubectl apply -f db-postgres.yaml
 helm install flyte flyteorg/flyte-core -n flyte -f flyte-values.yaml
 
